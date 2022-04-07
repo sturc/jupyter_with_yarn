cbf606a32e08d4cb24231901a4c43175a253757e
Christoph Sturm <43139608+sturc@users.noreply.github.com>
2022-04-07 15:41:43 +0200

    upgraded spark to 3.2.1 upgraded to java 11

--- build_pyspark-notebook.sh
+++ build_pyspark-notebook.sh
@@ -5,9 +5,13 @@ export JUPYTER_ENABLE_LAB=yes
 
 docker build \
     https://github.com/jupyter/docker-stacks.git#master:pyspark-notebook \
-    -t csturm/pyspark-notebook:winter-term-2021 \
-    --build-arg spark_version=3.1.2 \
+    -t csturm/pyspark-notebook:summer-term-2022 \
+    --build-arg spark_version=3.2.1 \
     --build-arg hadoop_version=2.7 \
-    --build-arg spark_checksum=BA47E074B2A641B23EE900D4E28260BAA250E2410859D481B38F2EAD888C30DAEA3683F505608870148CF40F76C357222A2773F1471E7342C622E93BF02479B7 \
-    --build-arg openjdk_version=8 \
-    --build-arg py4j_version=0.10.9
+    --build-arg spark_checksum=2EC9F1CB65AF5EE7657CA83A1ABACA805612B8B3A1D8D9BB67E317106025C81BA8D44D82AD6FDB45BBE6CAA768D449CD6A4945EC050CE9390F806F46C5CB1397 \
+    --build-arg openjdk_version=11 
+ #   --build-arg py4j_version=0.10.9
+
+# settings for Hadoop 3.3
+ #--build-arg hadoop_version=3.3 \
+ #   --build-arg spark_checksum=145ADACF189FECF05FBA3A69841D2804DD66546B11D14FC181AC49D89F3CB5E4FECD9B25F56F0AF767155419CD430838FB651992AEB37D3A6F91E7E009D1F9AE \
