f8c858162abfebe422f600f2d9e24de66bf1b99e
Christoph Sturm <43139608+sturc@users.noreply.github.com>
2022-04-19 22:36:14 +0200

    changed to hadoop 3.2

--- build_hadoop-python.sh
+++ build_hadoop-python.sh
@@ -4,6 +4,6 @@
 # docker buildx create --name mymultiarchbuilder --use 
 
 docker buildx build --platform linux/amd64,linux/arm64 \
-    -t csturm/hadoop-python:h2.10-p3.8.12-j11 \
+    -t csturm/hadoop-python:h3.2-p3.9.10-j11 \
     --push \
     hadoop 
--- build_pyspark-notebook.sh
+++ build_pyspark-notebook.sh
@@ -8,14 +8,9 @@ export JUPYTER_ENABLE_LAB=yes
 
 docker buildx build --platform linux/amd64,linux/arm64 \
     https://github.com/jupyter/docker-stacks.git#master:pyspark-notebook \
-    -t csturm/pyspark-notebook:spark3.2.1_java11 \
-    --push \
+    -t csturm/pyspark-notebook:spark3.2.1_hadoop_3.2_java11 \
     --build-arg spark_version=3.2.1 \
-    --build-arg hadoop_version=2.7 \
-    --build-arg spark_checksum=2EC9F1CB65AF5EE7657CA83A1ABACA805612B8B3A1D8D9BB67E317106025C81BA8D44D82AD6FDB45BBE6CAA768D449CD6A4945EC050CE9390F806F46C5CB1397 \
-    --build-arg openjdk_version=11
- #   --build-arg py4j_version=0.10.9
-
-# settings for Hadoop 3.3
- #--build-arg hadoop_version=3.3 \
- #   --build-arg spark_checksum=145ADACF189FECF05FBA3A69841D2804DD66546B11D14FC181AC49D89F3CB5E4FECD9B25F56F0AF767155419CD430838FB651992AEB37D3A6F91E7E009D1F9AE \
+    --build-arg hadoop_version=3.2 \
+    --build-arg spark_checksum=145ADACF189FECF05FBA3A69841D2804DD66546B11D14FC181AC49D89F3CB5E4FECD9B25F56F0AF767155419CD430838FB651992AEB37D3A6F91E7E009D1F9AE \
+    --build-arg openjdk_version=11 \
+    --push 
