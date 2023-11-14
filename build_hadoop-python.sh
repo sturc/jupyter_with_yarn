7eff6e1d1286aa31f60d7a35e4fda4b2542c5eea
Christoph Sturm <43139608+sturc@users.noreply.github.com>
2023-11-14 21:41:38 +0100

    updated build command

--- build_hadoop-python.sh
+++ build_hadoop-python.sh
@@ -3,7 +3,10 @@
 # creation of a new builder needed to enabel multiarch feature 
 # docker buildx create --name mymultiarchbuilder --use 
 
+# switching to existing builder 
+# docker buildx use mymultiarchbuilder
+
 docker buildx build --platform linux/amd64,linux/arm64 \
-    -t csturm/hadoop-python:h3.2-p3.9.10-j11 \
+    -t csturm/hadoop-python:h3.3.6-p3.9.10-j11 \
     --push \
     hadoop 
