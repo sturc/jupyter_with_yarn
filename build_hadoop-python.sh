09829080a38e6c51a13fa48090f7eb47998dcec1
Christoph Sturm <43139608+sturc@users.noreply.github.com>
2024-11-18 15:54:15 +0100

    upgrade to spark 3.5.3

--- build_hadoop-python.sh
+++ build_hadoop-python.sh
@@ -7,6 +7,6 @@
 # docker buildx use mymultiarchbuilder
 
 docker buildx build --platform linux/amd64,linux/arm64 \
-    -t csturm/hadoop-python:h3.3.6-p3.9.10-j11 \
+    -t csturm/hadoop-python:h3.3.6-p3.9.20-j11 \
     --push \
     hadoop 
